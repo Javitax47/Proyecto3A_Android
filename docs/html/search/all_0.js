bc11c392fb81f5c8a02a510f0c56e5ed096a1c2d
Javitax47 <camarenacuartero@gmail.com>
2024-09-27 21:47:45 +0200

    Documentación doxygen actualizada

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['actualizarip_0',['actualizarIP',['../classcom_1_1example_1_1usuario__upv_1_1proyecto3a_1_1_main_activity.html#a2142f848a202d56733b7b6a24ad368b0',1,'com::example::usuario_upv::proyecto3a::MainActivity']]]
+  ['actualizarip_0',['actualizarIP',['../classcom_1_1example_1_1usuario__upv_1_1proyecto3a_1_1_main_activity.html#a2142f848a202d56733b7b6a24ad368b0',1,'com::example::usuario_upv::proyecto3a::MainActivity']]],
+  ['actualizaruuid_1',['actualizarUuid',['../classcom_1_1example_1_1usuario__upv_1_1proyecto3a_1_1_main_activity.html#a6cf7ec4f2c81a3e7281aefb760872c74',1,'com::example::usuario_upv::proyecto3a::MainActivity']]]
 ];
