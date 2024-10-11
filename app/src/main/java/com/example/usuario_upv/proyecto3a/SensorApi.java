d517afcd213b9a55b91d1d7151e06d188d4041cf
Javitax47 <camarenacuartero@gmail.com>
2024-10-11 11:20:11 +0200

    Eliminada lógica de negocio sin usar

--- app/src/main/java/com/example/usuario_upv/proyecto3a/SensorApi.java
+++ app/src/main/java/com/example/usuario_upv/proyecto3a/SensorApi.java
@@ -33,29 +33,6 @@ public interface SensorApi {
     Call<Void> checkConnection();
 
     /**
-     * @brief Crea las tablas necesarias en la base de datos.
-     *
-     * Este método envía una solicitud GET al endpoint de configuración para
-     * crear las tablas requeridas.
-     *
-     * @return Un objeto Call que contiene la respuesta de la solicitud.
-     */
-    @GET("setup")
-    Call<Void> setupTables();
-
-    /**
-     * @brief Inserta un nuevo usuario en la base de datos.
-     *
-     * Este método envía una solicitud POST con la información del usuario
-     * en el cuerpo de la solicitud.
-     *
-     * @param user El objeto User que contiene la información del nuevo usuario.
-     * @return Un objeto Call que contiene la respuesta de la solicitud.
-     */
-    @POST("users")
-    Call<Void> createUser(@Body User user);
-
-    /**
      * @brief Inserta una medición de sensor en la base de datos.
      *
      * Este método envía una solicitud POST con la información de la medición
@@ -66,27 +43,4 @@ public interface SensorApi {
      */
     @POST("/")
     Call<Void> createSensorData(@Body SensorData sensorData);
-
-    /**
-     * @brief Elimina un usuario de la base de datos por su ID.
-     *
-     * Este método envía una solicitud DELETE al endpoint correspondiente para
-     * eliminar al usuario especificado.
-     *
-     * @param userId El ID del usuario que se desea eliminar.
-     * @return Un objeto Call que contiene la respuesta de la solicitud.
-     */
-    @DELETE("users/{id}")
-    Call<Void> deleteUser(@Path("id") int userId);
-
-    /**
-     * @brief Resetea las tablas en la base de datos.
-     *
-     * Este método envía una solicitud DELETE al endpoint para reiniciar
-     * las tablas.
-     *
-     * @return Un objeto Call que contiene la respuesta de la solicitud.
-     */
-    @DELETE("reset")
-    Call<Void> resetTables();
 }
