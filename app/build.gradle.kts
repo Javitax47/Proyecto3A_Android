04496f32e86d6d198ee3f4a8442635498dfa6de7
Javitax47 <camarenacuartero@gmail.com>
2024-10-27 02:14:10 +0200

    Registro, inicio de sesión y layout en tabs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -39,5 +38,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-location:17.1.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.android.volley:volley:1.2.1")
 }
